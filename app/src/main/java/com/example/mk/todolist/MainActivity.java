9b8a4dfd3827c8dd57e9ad58493892144da00047
Mk <mkhalil.1993@gmail.com>
2016-08-20 13:54:35 +0200

    Finsihed the intial interface fixed the problem with spinner

--- app/src/main/java/com/example/mk/todolist/MainActivity.java
+++ app/src/main/java/com/example/mk/todolist/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -37,13 +38,26 @@ public class MainActivity extends AppCompatActivity {
         final Button SaveButton = (Button) findViewById(R.id.save);
         final Button CancelButton = (Button) findViewById(R.id.cancel);
 
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerItems, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        importance.setAdapter(adapter);
+
         //change deponding on selected item
         importance.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
         {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
             {
                 String selectedItem = parent.getItemAtPosition(position).toString();
-                importance.setSelection(position);
+                Context context = getApplicationContext();
+                CharSequence text = "the slected item is " + selectedItem;
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
 
             } // to close the onItemSelected
             public void onNothingSelected(AdapterView<?> parent)
